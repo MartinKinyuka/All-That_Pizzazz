020f76cc4e47c816a7a2dfae5b80475b75f6d8db
MartinKinyuka <mkinyuka@gmail.com>
2021-04-26 20:17:15 +0300

    append order option added

--- js/script.js
+++ js/script.js
@@ -4,14 +4,31 @@ $(document).ready(function(){
     var toppings =$("#toppings option:selected").val();
     var crust =$("#crust option:selected").val();
   
-$("#orderTable").toggle();
-$("#processButtons").toggle();
+$("#orderTable").show();
+$("#processButtons").show();
+$("#add").show();
+$("#submission").hide();
+
 
     
     
 $("#sizes").html(size)
 $("#tops").html(toppings)
 $("#crusts").html(crust)
+
+$("#add").on("click", function(){
+  var size =$("#size option:selected").val();
+  var toppings =$("#toppings option:selected").val();
+  var crust =$("#crust option:selected").val();
+  
+  $('#orderTable').append($('<tr>')
+    .append($('<td>').append(size))
+    .append($('<td>').append(toppings))
+    .append($('<td>').append(crust))
+    
+  )
+  
+})
     
    
   })
